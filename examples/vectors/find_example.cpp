66249225bda25bdf027afd97267838a99c155245
Michael Zingale <michael.zingale@stonybrook.edu>
2022-02-20 10:36:18 -0500

    add an example on find

---
+++ examples/vectors/find_example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+    std::vector<int> container{100, 200, 300, 400, 500, 600};
+
+    // search through the entire vector to find the first instance of the
+    // element "400"
+
+    auto pos = find(container.cbegin(), container.cend(), 400);
+
+    std::cout << "element found: " << *pos << std::endl;
+
+    // output the remaining elements after the one we searched for
+
+    for (auto it = pos+1; it < container.cend(); ++it) {
+        std::cout << *it << std::endl;
+    }
+}
