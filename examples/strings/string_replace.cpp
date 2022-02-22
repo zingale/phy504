5aab15cf6d63b16d4cad1bc2da49b792279c1967
Michael Zingale <michael.zingale@stonybrook.edu>
2022-02-22 14:18:03 -0500

    add examples

---
+++ examples/strings/string_replace.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string filename{"~/classes/test.cpp"};
+
+    // let's find just the base file name -- reverse find for '/'
+
+    auto ipos = filename.rfind('/');
+
+    // create a substring containing just the base name
+    // if we don't provide a lenght, substr goes to the end
+
+    std::string basename = filename.substr(ipos+1);
+
+    std::cout << "basename = " << basename << std::endl;
+
+    // now let's change the extension from .cpp to .txt
+
+    auto ipos_ext = basename.rfind('.');
+
+    // there are many forms of replace -- here's well use iterators that mark
+    // the start and end of the original string and those of the string we want
+    // to substitute
+    
+    std::string new_ext = ".txt";
+
+    basename.replace(basename.begin()+ipos_ext, basename.end(),
+                     new_ext.begin(), new_ext.end());
+
+    std::cout << "basename = " << basename << std::endl;
+
+}
