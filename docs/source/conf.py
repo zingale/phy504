8d3d38a90324675e45f514b1e706639000f8ffdc
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-07 13:01:20 -0500

    add panels

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinxcontrib.bibtex',
               'sphinx.ext.autosummary',
+              'sphinx_panels',
               'numpydoc',
               'sphinx.ext.githubpages',
               'sphinx_copybutton',
@@ -138,14 +139,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        '_static/css/theme.css',
-        '_static/copybutton.css',
-        '_static/pygments.css'
-    ],
-}
+html_css_files = ["theme_overrides.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
