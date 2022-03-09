362ce6b9dd458187c3f6e407f1faacbe7c550cf6
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-09 08:25:05 -0500

    update

---
+++ examples/functions/algorithms_functions_lambda.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+
+    std::vector<std::string> titles{"a new home",
+                                    "the empire strikes back",
+                                    "return of the jedi",
+                                    "the phantom menace", 
+                                    "attack of the clones",
+                                    "revenge of the sith",
+                                    "the force awakens",
+                                    "the last jedi",
+                                    "the rise of skywalker"};
+
+    std::sort(titles.begin(), titles.end());
+
+    for (auto e : titles) {
+        std::cout << e << std::endl;
+    }                                    
+    std::cout << std::endl;
+
+    // now sort by string length
+
+    std::sort(titles.begin(), titles.end(),
+              [] (const std::string& a, const std::string& b) {return a.size() < b.size();});
+
+    for (auto e : titles) {
+        std::cout << e << std::endl;
+    }                                    
+    std::cout << std::endl;
+
+}
