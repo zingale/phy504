d5a190c4d058adfb8cecadce9e309264c70e7b60
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-12 15:41:20 -0500

    add a class example

--- examples/contiguous_array/array.cpp
+++ examples/contiguous_array/array.cpp
@@ -2,43 +2,11 @@
 #include <iostream>
 #include <cassert>
 
-// a contiguous 2-d array
-// here the data is stored in row-major order in a 1-d memory space
-// managed as a vector.  We overload () to allow us to index this as
-// a(irow, icol)
+#include "array.H"
 
-struct Array {
-
-    std::vector<double> _data;
-    std::size_t _rows;
-    std::size_t _cols;
-    Array (std::size_t rows, std::size_t cols, double val=0.0)
-        : _rows{rows},
-          _cols{cols}
-          //_data(rows * cols, val)
-    {
-        assert (rows > 0);
-        assert (cols > 0);
-
-        // we do the initialization of _data here so we can first assert
-        // on the rows and cols.  If we placed it in the initialization list,
-        // then it would be created before we could check.
-
-        _data.resize(rows * cols, val);
-    }
-    
-    // note the "const" after the argument list here -- this means
-    // that this can be called on a const Array, while the first
-    // "const" refers to the return type
-
-    const std::size_t ncols() const { return _cols;}
-    const std::size_t nrows() const { return _rows;}
-
-    double& operator()(int row, int col);
-    const double& operator()(int row, int col) const;
-
-};
-
+// notice that we can put the function body for these member functions
+// outside of the class definition.  We just need to prefix them as
+// Array::
 
 double& Array::operator()(int row, int col) {
     assert (row >= 0 && row < _rows);
@@ -54,6 +22,9 @@ const double& Array::operator()(int row, int col) const {
     return _data[row*_cols + col];
 }  
 
+
+// the << operator is not part of the of the class, so it is not a member
+
 std::ostream& operator<< (std::ostream& os, const Array& a) {
 
     for (int row=0; row < a.nrows(); ++row) {
@@ -64,23 +35,4 @@ std::ostream& operator<< (std::ostream& os, const Array& a) {
     }
 
     return os;
-}
-
-int main() {
-
-    Array x(10, 10);
-
-    for (int row=0; row < x.nrows(); ++row) {
-        for (int col=0; col < x.ncols(); ++col) {
-            if (row == col) {
-                x(row, col) = 1.0;
-            }
-        }
-    }
-
-    std::cout << x << std::endl;
-
-    // this will fail the assertion
-    std::cout << x(11, 9);
-
 }
---
+++ examples/contiguous_array/test_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "array.H"
+
+int main() {
+
+    Array x(10, 10);
+
+    for (int row=0; row < x.nrows(); ++row) {
+        for (int col=0; col < x.ncols(); ++col) {
+            if (row == col) {
+                x(row, col) = 1.0;
+            }
+        }
+    }
+
+    std::cout << x << std::endl;
+
+    Array y(4, 3);
+
+    double c{0};
+    for (auto &e : y._data) {
+        e = c;
+        c += 1;
+    }
+
+    std::cout << y << std::endl;
+    
+    // this will fail the assertion
+    std::cout << x(11, 9);
+
+}
