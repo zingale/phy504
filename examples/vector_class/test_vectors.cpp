ba9101791452c0f1fc5b97cf12122d5ea8ba323f
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-24 09:35:54 -0400

    add a vector example

---
+++ examples/vector_class/test_vectors.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "vector2d.H"
+
+int main() {
+
+    Vector2d v1(1, 2);
+    Vector2d v2(2, 4);
+
+    std::cout << v1 << " " << v2 << std::endl;
+    std::cout << v1 + v2 << std::endl;
+
+    auto v3 = v1 - v2;
+    std::cout << v3 << std::endl;
+
+    auto v4 = v3;
+    std::cout << v3 << " " << v4 << std::endl;
+    v3.x = 0.0;
+    v3.y = 0.0;
+    std::cout << v3 << " " << v4 << std::endl;
+}
