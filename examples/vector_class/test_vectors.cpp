60954c06c8b01897e2a63f06cfa2f57ccc9aedb5
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-24 17:53:58 -0400

    updates to style

--- examples/vector_class/test_vectors.cpp
+++ examples/vector_class/test_vectors.cpp
@@ -3,21 +3,35 @@
 
 int main() {
 
+    // create 2 vectors
+
     Vector2d v1(1, 2);
     Vector2d v2(2, 4);
 
+    // output our vectors
+
     std::cout << v1 << " " << v2 << std::endl;
+
+    // output their sum
+
     std::cout << v1 + v2 << std::endl;
+
+    // create a new vector from subracting our two vectors
 
     auto v3 = v1 - v2;
     std::cout << v3 << std::endl;
 
+    // create a copy
+
     auto v4 = v3;
     std::cout << v3 << " " << v4 << std::endl;
+
+    // change the data in the original
     v3.set_x(0.0);
     v3.set_y(0.0);
-    std::cout << v3 << " " << v4 << std::endl;
 
-    auto v5 = -v3;
+    // did both change? or just the original?
+
+    std::cout << v3 << " " << v4 << std::endl;
 
 }
