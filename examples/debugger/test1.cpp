9f2e525ccd87d540c2144881d4699cb093adafce
Michael Zingale <michael.zingale@stonybrook.edu>
2022-04-07 18:04:49 -0400

    add test

---
+++ examples/debugger/test1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+double f(double x) {
+    return std::sin(x) * std::sin(x);
+}
+
+
+void integrate(int N, double xmin, double xmax, double& I) {
+
+    double dx = (xmax - xmin) / N;
+
+    double xleft{xmin};
+    double fleft = f(xleft);
+
+    double xright{0.0};
+    double fright{0.0};
+
+    for (int n = 0; n < N; ++n) {
+
+        xright = xmin + (n+1) * dx;
+        fright = f(xright);
+
+        I += 1./2 * dx * (fleft + fright);
+
+    }
+
+}
+
+int main() {
+
+    int N{-1};
+
+    std::cout << "enter the number of intervals: ";
+    std::cin >> N;
+
+    assert(N > 0);
+
+    double I{0.0};
+
+    double xmin{0.0};
+    double xmax{M_PI};
+
+    integrate(N, xmin, xmax, I);
+    
+    std::cout << "integral is " << I << std::endl;
+
+}
