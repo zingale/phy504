c34abcd64bb4440acb2d7b4f290081181ab44395
Michael Zingale <michael.zingale@stonybrook.edu>
2022-04-10 15:34:17 -0400

    add more files

---
+++ examples/more_classes/more_test_vectors.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "vector2d.H"
+
+int main() {
+
+    Vector2d v1(1.0, 2.0);
+    Vector2d v2, v3;
+
+    // test assignment
+
+    std::cout << "doing v3 = v2 = v1" << std::endl;
+
+    v3 = v2 = v1;
+
+    std::cout << v2 << " " << v3 << std::endl;
+
+    // test copy
+
+    std::cout << "doing auto v4 = v1" << std::endl;
+    auto v4 = v1;
+
+    std::cout << v4 << std::endl;
+
+}
