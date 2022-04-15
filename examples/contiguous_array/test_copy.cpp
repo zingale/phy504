a9dd8c115885afd2b47beb793631a3052138be67
Michael Zingale <michael.zingale@stonybrook.edu>
2022-04-15 13:47:27 -0400

    demonstrate a copy

---
+++ examples/contiguous_array/test_copy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "array.H"
+
+int main() {
+
+    Array a(10, 10);
+
+    auto b = a;
+    
+    b(1,1) = 10.0;
+
+    std::cout << a << std::endl;
+
+    std::cout << b << std::endl;
+}
