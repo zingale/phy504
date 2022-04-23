01f9f30a65a2d137463b9d1675cfb52082edaf4a
Michael Zingale <michael.zingale@stonybrook.edu>
2022-04-23 11:48:43 -0400

    add a Poisson solver

--- examples/numerical_algorithms/poisson/poisson.cpp
+++ examples/numerical_algorithms/poisson/poisson.cpp
@@ -1,67 +1,17 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
+#include "poisson.H"
 
-///
-/// Solve the 1-d Poisson problem with Dirichlet BCs using Gauss-Seidel smoothing
-///
-class Poisson {
+const double TOL = 1.e-10;
 
-private:
+int main() {
 
-    double xmin;
-    double xmax;
+    auto p = Poisson(0.0, 1.0, 64);
 
-    int N;
+    p.set_source([] (double x) {return std::sin(x);});
 
-    std::vector<double> x;
-    std::vector<double> phi;
-    std::vector<double> f;
-    
-    double dx;
+    p.set_left_bc(0.0);
+    p.set_right_bc(1.0);
 
-public:
+    p.solve(TOL);
 
-    Poisson(double xmin_in, double xmax_in, int N_in)
-        : xmin{xmin_in}, xmax{xmax_in}, N{N_in},
-          x(N_in, 0), phi(N_in, 0), f(N_in, 0)
-    {
-
-        // initialize the coordinates
-        assert (xmax > xmin);
-
-        dx = (xmax - xmin) / static_cast<double>(N-1);
-
-        for (int i = 0; i < N; ++i) {
-            x[i] = xmin + static_cast<double>(i) * dx;
-        }
-
-    }
-
-    void set_left_bc(double val) {x[0] = val;}
-
-    void set_right_bc(double val) {x[N-1] = val;}
-
-    std::vector<double>& get_source() {
-        return f;
-    }
-
-    std::vector<double>& get_phi() {
-        return phi;
-    }
-
-    void smooth(int n_smooth) {
-
-        // perform Gauss-Seidel smoothing
-
-        // we only operate on the interior nodes
-
-        for (int i = 0; i < n_smooth; ++i) {
-
-            for (int j = 1; j < N-1; ++j) {
-                x[j] = 0.5 * (x[j-1] + x[j+1] - dx * dx * f[j]);
-            }
-        }
-    }
-
-};
+    p.write_solution("poisson.txt");
+}
