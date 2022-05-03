e893cd8150b542ee6a2dfa7b4f756f064b7d6d20
Michael Zingale <michael.zingale@stonybrook.edu>
2022-05-02 21:28:04 -0400

    updates

--- examples/parallel/openmp/random_pi/pi.cpp
+++ examples/parallel/openmp/random_pi/pi.cpp
@@ -4,21 +4,32 @@
 #include <random>
 #include <cmath>
 
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 
 const int N = 1000000;
 
 int main() {
 
+#ifdef _OPENMP
     auto start = omp_get_wtime();
+#endif
 
     int N_circle{0};
 
     #pragma omp parallel reduction(+:N_circle)
     {
 
+        // each thread will have its own random number
+        // engine
+
         std::random_device rd;
+#ifdef _OPENMP
         std::seed_seq seed{static_cast<int>(rd()), omp_get_thread_num()};
+#else
+        std::seed_seq seed{static_cast<int>(rd())};
+#endif
         std::mt19937 generator(seed);
         std::uniform_real_distribution<double> uniform(0.0, 1.0);
 
@@ -35,10 +46,13 @@ int main() {
             }
 
         }
-        
+
     }
 
     std::cout << "pi = " << std::setprecision(10) << 4 * static_cast<double>(N_circle) / N << std::endl;
 
+#ifdef _OPENMP
     std::cout << "time = " << omp_get_wtime() - start << std::endl;
+#endif
+
 }
