2b0fa0dd4ac3d4c663478fe8303a0894760ecc3a
Michael Zingale <michael.zingale@stonybrook.edu>
2024-02-09 10:05:09 -0500

    fix limits

--- examples/limits/limits_test.cpp
+++ examples/limits/limits_test.cpp
@@ -9,14 +9,8 @@ int main() {
     std::cout << "  largest value = " << std::numeric_limits<double>::max() << std::endl;
 
     std::cout << "short: " << std::endl;
-    std::cout << "  smallest value = " << std::numeric_limits<unsigned short>::min() << std::endl;
-    std::cout << "  lowest value = " << std::numeric_limits<unsigned short>::lowest() << std::endl;
-    std::cout << "  largest value = " << std::numeric_limits<unsigned short>::max() << std::endl;
-
-    int i = 32767;
-    std::cout << "i = " << i << std::endl;
-    i++;
-    std::cout << "i = " << i << std::endl;
-
+    std::cout << "  smallest value = " << std::numeric_limits<short>::min() << std::endl;
+    std::cout << "  lowest value = " << std::numeric_limits<short>::lowest() << std::endl;
+    std::cout << "  largest value = " << std::numeric_limits<short>::max() << std::endl;
 
 }
