4860751de025169297386541ab86932953abaccd
Michael Zingale <michael.zingale@stonybrook.edu>
2024-02-20 15:04:56 -0500

    add a fallthrough example

---
+++ examples/compiler-warnings/fallthrough.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    double x{};
+    std::string sign{};
+
+    std::cout << "enter a number: ";
+    std::cin >> x;
+
+    const auto sgn = static_cast<int>(std::copysign(1.0, x));
+
+    switch (sgn) {
+
+    case -1:
+	sign = "negative";
+    case 0:
+	sign = "none";
+	break;
+    default:
+	sign = "positive";
+    }
+
+    std::cout << "sign = " << sign << std::endl;
+}
