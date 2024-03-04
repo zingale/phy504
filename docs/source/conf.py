423ad629109f46241935b2cc9daacad3ec38f4fa
Michael Zingale <michael.zingale@stonybrook.edu>
2024-03-04 10:09:11 -0500

    fix dollarmath

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
+              'sphinx_math_dollar',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinxcontrib.bibtex',
