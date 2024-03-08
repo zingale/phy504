c683cc5bed1ef46b96ca3a9d68ca88df4431cdf6
Michael Zingale <michael.zingale@stonybrook.edu>
2024-03-08 08:39:17 -0500

    change the default theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ language = "en"
 exclude_patterns = ['**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
