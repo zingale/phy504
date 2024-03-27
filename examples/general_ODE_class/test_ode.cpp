7d5deac8b6198a6b5cfb2c473b87f189c4681b62
Michael Zingale <michael.zingale@stonybrook.edu>
2024-03-27 13:47:25 -0400

    this works well

--- examples/general_ODE_class/test_ode.cpp
+++ examples/general_ODE_class/test_ode.cpp
@@ -1,14 +1,34 @@
 #include <array>
+#include <iostream>
 
 #include "ode_integrator.H"
 
 constexpr int N{4};
 
-std::array<double, N> orbit(double t, const std::array<double, N>& y) {
+enum orbit_comps {
+    ix,
+    iy,
+    iu,
+    iv};
+
+constexpr double GM{4.0 * M_PI * M_PI};   // G * Mass in AU, year, solar mass unit
+
+std::array<double, N> orbit([[maybe_unused]] 
+double t, const std::array<double, N>& y) {
+
+    // order is x, y, u, v
 
     std::array<double, N> dydt{};
 
-    dydt[0] = -y[0];
+    dydt[ix] = y[iu];
+    dydt[iy] = y[iv];
+
+    // du/dt = - GMx/r**3; dv/dt = - GMy/r**3
+
+    double r = std::sqrt(y[ix] * y[ix] + y[iy] * y[iy]);
+
+    dydt[iu] = - GM * y[ix] / std::pow(r, 3);
+    dydt[iv] = - GM * y[iy] / std::pow(r, 3);
 
     return dydt;
 
@@ -16,9 +36,13 @@ std::array<double, N> orbit(double t, const std::array<double, N>& y) {
 
 int main() {
 
-    ODE<N> o(orbit, {1.0, 0.0, 0.0, 0.0});
+    double a{1.0};
+    double e{0.6};
 
-    auto trajectory = o.integrate(0.1, 1.0);
+    ODE<N> o(orbit, {a * (1.0 - e), 0.0,
+                     0.0, std::sqrt(GM / a * (1.0 + e) / (1.0 - e))});
+
+    auto trajectory = o.integrate(0.025, 1.0);
 
     for (auto s : trajectory) {
         std::cout << s.t << " ";
